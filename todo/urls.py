681b49784d8699dc849bc1f2167346922480e6c9
julianvr0408 <julianvr022006@gmail.com>
2025-04-03 20:37:09 -0500

    coment

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path,include
-from.import views
+from django.urls import path
+from todo import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path("",views.home,name="home"),
+    path('admin/', admin.site.urls),
+    path("", views.home,name="home"),
+    path("agregar/",views.agregar,name="agregar"),
+
 ]
